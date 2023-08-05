9b072e6a166165ec562c60555d769c6b4b5d8b6a
Joe Porembski <jporembski@gmail.com>
2023-08-05 16:47:02 -0400

    Complete initial bring up

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,14 @@
 Copyright (c) 2023 Joe Porembski
 SPDX-License-Identifier: BSD-3-Clause
 ------------------------------------------------------------------------------*/
+#include "generated/configuration.hpp"
+#include "gpio.hpp"
+
+#include <hardware/gpio.h>
 
 int main(int argc, char** argv)
 {
+    gpio_init(SYSTEM_LED_PIN);
+    gpio_set_dir(SYSTEM_LED_PIN, GPIO_OUT);
+    gpio_put(SYSTEM_LED_PIN, ON);
 }
