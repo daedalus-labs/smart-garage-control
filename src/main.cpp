0b6effcd00f7f5e8081f73ebf91f5dc62bf25e39
Joe Porembski <jporembski@gmail.com>
2023-09-23 17:53:16 -0400

    Add the MQTT Client (#8)
    This commit integrates the `pico-mqtt` submodule.
    
    Resolves #1

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ SPDX-License-Identifier: BSD-3-Clause
 #include "generated/configuration.hpp"
 #include "gpio.hpp"
 
+#include "mqtt.hpp"
+
 #include <hardware/gpio.h>
 
 int main(int argc, char** argv)
@@ -12,4 +14,6 @@ int main(int argc, char** argv)
     gpio_init(SYSTEM_LED_PIN);
     gpio_set_dir(SYSTEM_LED_PIN, GPIO_OUT);
     gpio_put(SYSTEM_LED_PIN, ON);
+
+    mqtt::Client mqtt("Fomalhaut", CONFIGURED_MQTT_PORT, "test", MQTT_FEEDBACK_PIN);
 }
