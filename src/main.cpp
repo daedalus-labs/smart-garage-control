9e0b96b6e52c2f32b96985ecfbe16f4a547b595d
Joe Porembski <jporembski@gmail.com>
2023-09-24 00:04:24 -0400

    Add configuration loading

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,85 @@ SPDX-License-Identifier: BSD-3-Clause
 ------------------------------------------------------------------------------*/
 #include "generated/configuration.hpp"
 #include "gpio.hpp"
-
 #include "mqtt.hpp"
+#include "utilities.hpp"
 
+#include <hardware/adc.h>
 #include <hardware/gpio.h>
+#include <pico/stdio.h>
 
-int main(int argc, char** argv)
+#include <cstdint>
+#include <string>
+
+inline constexpr uint32_t COMMUNICATION_PERIOD_MS = 10000;
+inline constexpr uint32_t MQTT_CONNECTION_WAIT_MS = 17500;
+
+static void initialize()
 {
+    stdio_init_all();
+    adc_init();
+
     gpio_init(SYSTEM_LED_PIN);
     gpio_set_dir(SYSTEM_LED_PIN, GPIO_OUT);
     gpio_put(SYSTEM_LED_PIN, ON);
+}
 
-    mqtt::Client mqtt("Fomalhaut", CONFIGURED_MQTT_PORT, "test", MQTT_FEEDBACK_PIN);
+static bool initializeMQTT(mqtt::Client& client, const std::string& uid)
+{
+    char mqtt_topic[TOPIC_BUFFER_SIZE];
+    snprintf(mqtt_topic, TOPIC_BUFFER_SIZE, VERSION_TOPIC_FORMAT.data(), client.deviceName().c_str());
+    if (!client.publish(mqtt_topic, static_cast<const void*>(VERSION.data()), VERSION.size(), mqtt::QoS::EXACTLY_ONCE, true)) {
+        printf("Failed to publish %s\n", mqtt_topic);
+        return false;
+    }
+
+    snprintf(mqtt_topic, TOPIC_BUFFER_SIZE, UID_TOPIC_FORMAT.data(), client.deviceName().c_str());
+    if (!client.publish(mqtt_topic, static_cast<const void*>(uid.c_str()), uid.size(), mqtt::QoS::EXACTLY_ONCE, true)) {
+        printf("Failed to publish %s\n", mqtt_topic);
+        return false;
+    }
+
+    printf("Successfully initialized MQTT\n");
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    initialize();
+    std::string board_id = systemIdentifier();
+    bool mqtt_initialized = false;
+    uint32_t count = 0;
+
+    sleep_ms(5000);
+    SystemConfiguration cfg;
+    if (!read(cfg)) {
+        printf("Failed to read system configuration\n");
+        return EXIT_FAILURE;
+    }
+
+    mqtt::Client client(cfg.mqttBroker(), CONFIGURED_MQTT_PORT, cfg.deviceName(), MQTT_FEEDBACK_PIN);
+
+    while (true) {
+        if (!client.connected()) {
+            printf("Connecting MQTT...\n");
+            mqtt_initialized = false;
+            client.connect();
+            sleep_ms(MQTT_CONNECTION_WAIT_MS);
+            continue;
+        }
+
+        if (!mqtt_initialized) {
+            printf("Initializing MQTT...\n");
+            mqtt_initialized = initializeMQTT(client, board_id);
+            sleep_ms(COMMUNICATION_PERIOD_MS);
+            continue;
+        }
+
+        printf("\n----------------- [%u]\n", count);
+        printf("MQTT Status: %s\n", client.connected() ? "true" : "false");
+        printf("-----------------\n");
+
+        sleep_ms(COMMUNICATION_PERIOD_MS);
+        count++;
+    }
 }
---
+++ src/utilities.hpp
@@ -0,0 +1,121 @@
+/*------------------------------------------------------------------------------
+Copyright (c) 2023 Joe Porembski
+SPDX-License-Identifier: BSD-3-Clause
+------------------------------------------------------------------------------*/
+#pragma once
+
+
+#include <pico/time.h>
+#include <pico/types.h>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+
+inline constexpr float C_TO_F_SCALE = 9.0 / 5.0;
+inline constexpr float C_TO_F_OFFSET = 32.0;
+
+/**
+ * Class encapsulating the configuration data for a device.
+ */
+class SystemConfiguration
+{
+public:
+    /** Constructor */
+    SystemConfiguration();
+
+    /**
+     * Constructor
+     *
+     * @param[in] configuration The configuration data read from flash memory.
+     */
+    SystemConfiguration(const std::vector<uint8_t>& configuration);
+
+    /**
+     * @return The device name to be used for MQTT communication.
+     */
+    const std::string& deviceName() const;
+
+    /**
+     * @return The list of pins to control the Garage Doors.
+     */
+    const std::vector<uint8_t>& doorPins() const;
+
+    /**
+     * @note The MQTT Broker can be an IPv4 address or a hostname. This should be passed through a DNS resolver.
+     * @return The MQTT Broker.
+     */
+    const std::string& mqttBroker() const;
+
+    /**
+     * @return The passphrase for the configured wireless network.
+     */
+    const std::string& passphrase() const;
+
+    /**
+     * @return The SSID of the configured wireless network.
+     */
+    const std::string& ssid() const;
+
+private:
+    /**
+     * Helper function to parse @a configuration.
+     *
+     * @param[in] configuration The configuration data read from flash memory.
+     */
+    void _parse(const std::vector<uint8_t>& configuration);
+
+    std::string _ssid;
+    std::string _passphrase;
+    std::string _mqtt_broker;
+    std::string _device_name;
+    std::vector<uint8_t> _door_pins;
+};
+
+/**
+ * @return The microseconds since boot.
+ */
+uint64_t microseconds();
+
+/**
+ * @return The milliseconds since boot.
+ */
+uint64_t milliseconds();
+
+/**
+ * @return The unique identifier of this system.
+ */
+std::string systemIdentifier();
+
+/**
+ * Waits on the to become @a desired_state.
+ *
+ * Will timeout after @a wait_length, and return false.
+ *
+ * @note This is a blocking wait.
+ * @param[in] gpio_pin The pin to wait on.
+ * @param[in] desired_state The desired state of @a gpio_pin.
+ * @param[in] wait_length The time to wait for it to enter this state in microseconds (us).
+ * @return True if the wait was successful, false otherwise.
+ */
+bool wait(uint8_t gpio_pin, bool desired_state, uint64_t wait_length);
+
+/**
+ * Reads the system configuration information from flash memory.
+ *
+ * @param[in] cfg The system configuration information to be read.
+ * @return True if the read was successful, false otherwise.
+ */
+bool read(SystemConfiguration& cfg);
+
+/**
+ * Converts a temperature in celsius to its equivalent in fahrenheit.
+ *
+ * @param[in] temperature The temperature in celsius.
+ * @return The value of @a temperature converted to fahrenheit.
+ */
+inline float toFahrenheit(float temperature)
+{
+    return (C_TO_F_SCALE * temperature) + C_TO_F_OFFSET;
+}
