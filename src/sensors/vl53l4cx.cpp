f64b517bdff6dc58643e78dbb110473ea83cb6a9
Joe Porembski <jporembski@gmail.com>
2023-10-01 20:26:30 -0400

    stash

--- src/sensors/vl53l4cx.cpp
+++ src/sensors/vl53l4cx.cpp
@@ -1,18 +1,65 @@
 /*------------------------------------------------------------------------------
 Copyright (c) 2023 Joe Porembski
 SPDX-License-Identifier: BSD-3-Clause
+
+This code uses reference code copyright 2022 by STMicroelectronics
 ------------------------------------------------------------------------------*/
 
 #include "vl53l4cx.hpp"
 
-namespace sensors {
-VL53L4CX::VL53L4CX()
-{}
+#include "gpio.hpp"
 
-VL53L4CX::~VL53L4CX() = default;
+#include <hardware/gpio.h>
+#include <hardware/i2c.h>
+
+#include <cstdint>
+
+inline constexpr uint8_t T_BOOT_MS = 3;
+
+
+namespace sensors {
+VL53L4CX::VL53L4CX(uint8_t address, uint8_t shutdown_pin) : _address(address), _shutdown_pin(shutdown_pin)
+{
+    gpio_init(_shutdown_pin);
+    gpio_set_dir(_shutdown_pin, GPIO_OUT);
+
+    // Turning the sensor off first to avoid any issues caused by
+    // the floating GPIO.
+    _off();
+    _on();
+}
+
+VL53L4CX::~VL53L4CX()
+{
+    _off();
+}
 
 int32_t VL53L4CX::distance() const
 {
     return 0;
+}
+
+bool VL53L4CX::start()
+{}
+
+bool VL53L4CX::_checkDeviceInitialized()
+{}
+
+bool VL53L4CX::_initializeData()
+{}
+
+bool VL53L4CX::_setDeviceAddress()
+{}
+
+void VL53L4CX::_off()
+{
+    gpio_put(_shutdown_pin, OFF);
+    sleep_ms(T_BOOT_MS);
+}
+
+void VL53L4CX::_on()
+{
+    gpio_put(_shutdown_pin, ON);
+    sleep_ms(T_BOOT_MS);
 }
 } // namespace sensors
--- src/sensors/vl53l4cx.hpp
+++ src/sensors/vl53l4cx.hpp
@@ -14,8 +14,20 @@ namespace sensors {
 class VL53L4CX final : public Range
 {
 public:
-    VL53L4CX();
+    VL53L4CX(uint8_t address, uint8_t shutdown_pin);
     ~VL53L4CX();
     int32_t distance() const override;
+    bool start();
+
+private:
+    bool _checkDeviceInitialized();
+    bool _initializeData();
+    bool _setDeviceAddress();
+
+    void _off();
+    void _on();
+
+    const uint8_t _address;
+    const uint8_t _shutdown_pin;
 };
 } // namespace sensors
